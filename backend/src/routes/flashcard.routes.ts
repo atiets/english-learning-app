e509d8cdf988a68e6795744705d8cb6b99c41cb5
atiets <nguyenanhtuyet03.nbk@gmail.com>
2026-03-14 18:02:41 +0700

    feat: complete flashcard CRUD API

--- backend/src/routes/flashcard.routes.ts
+++ backend/src/routes/flashcard.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { createFlashcard, getFlashcards } from "../controllers/flashcard.controller.js";
+import { createFlashcard, getFlashcards, getFlashcardById, updateFlashcard, deleteFlashCard } from "../controllers/flashcard.controller.js";
 
 const router = Router();
 
 router.post("/", createFlashcard);
 router.get("/", getFlashcards);
+router.get("/:id", getFlashcardById);
+router.put("/:id", updateFlashcard);
+router.delete("/:id", deleteFlashCard);
 
 export default router;
